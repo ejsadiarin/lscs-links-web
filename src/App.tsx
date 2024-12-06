77ac52ebc31b46db5e4ac64d4fe622df7a202f9e
mbchavez27 <=epikstrykermaxchavez23@gmail.com>
2024-12-06 16:30:47 +0800

    add: dist

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Layout } from "./layout/Layout";
 import { BrowserRouter as Router } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Toaster } from "@/components/ui/toaster";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <GoogleOAuthProvider clientId="742104294908-maet14jq2ohqpl0uu0ma4qpllc30t0k5.apps.googleusercontent.com">
           <Router>
             <Layout></Layout>
+            <Toaster />
           </Router>
         </GoogleOAuthProvider>
       </CookiesProvider>
